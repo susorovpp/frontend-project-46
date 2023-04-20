d50340e1be58086633e7e67fb301483f1fe40f09
“FINE\psusorov” <pavel.susorov@abbyy.com>
2023-04-21 00:02:27 +0300

    add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2, options) => {
     const result = genDiff(filepath1, filepath2, options.format);
